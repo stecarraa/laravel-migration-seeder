0435d63d54bc7772a9fab7e0128b1d593f982e41
stecarraa <stefano@caratozzolo.me>
2022-09-10 00:15:40 +0200

    feat : print train on screen ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,17 @@
 
 @section("main-content")
 
+<ol>
+    @forelse ($trains as $train)
     
+   <li>
+    The departure of train from : <strong>{{$train->where_from}} </strong> To :  <strong>{{$train->where_for}}</strong> is scheduled for : <strong>{{$train->departure}}</strong>
+   </li>
 
+@empty
+    
+    <p>No train found</p>
+
+@endforelse
+</ol>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', "TrainsController@index")->name("home");
+
